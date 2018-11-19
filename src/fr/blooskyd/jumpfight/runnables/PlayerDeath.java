19e065e198554251966f807a18e796c9f9c0898c
BlooSkyd <clment2402@gmail.com>
2018-11-19 13:30:13 +0100

    ajout de la class playerDeath

--- src/fr/blooskyd/jumpfight/runnables/PlayerDeath.java
+++ src/fr/blooskyd/jumpfight/runnables/PlayerDeath.java
@@ -1,4 +1,13 @@
 package fr.blooskyd.jumpfight.runnables;
 
+import fr.ziedelth.gameframework.GameFramework;
+import fr.ziedelth.gameframework.utils.FrameworkPlayer;
+import fr.ziedelth.gameframework.utils.PlayerUtils;
+import fr.blooskyd.jumpfight.runnables.PlayerDeath;
+import fr.blooskyd.jumpfight.JumpFight;
+import fr.ziedelth.oneshot.managers.InventoryManager;
+import org.bukkit.entity.Player;
+
+
 public class PlayerDeath {
 }
