05b55843387b761069783135321ed8c74c3265d5
Ziedelth <45169338+Ziedelth@users.noreply.github.com>
2018-11-19 13:28:22 +0100

    Create InventoryManager

---
+++ src/fr/blooskyd/jumpfight/managers/InventoryManager.java
@@ -0,0 +1,9 @@
+package fr.blooskyd.jumpfight.managers;
+
+import org.bukkit.entity.Player;
+
+public class InventoryManager {
+	// Méthode appellée lorsque l'on veut modifié l'inventaire du joueur
+	public InventoryManager(Player player) {
+	}
+}
