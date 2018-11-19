bd9e0ac4dc590f90f14231f7e2a34f170b7ca48b
BlooSkyd <clment2402@gmail.com>
2018-11-19 13:12:58 +0100

    Ajout de quelques galères

--- src/fr/blooskyd/jumpfight/JumpFight.java
+++ src/fr/blooskyd/jumpfight/JumpFight.java
@@ -1,14 +1,21 @@
 package fr.blooskyd.jumpfight;
 
 import fr.ziedelth.dataclient.utils.Games;
+import fr.ziedelth.evolzapi.spigot.EvolzSpigot;
 import fr.ziedelth.gameframework.GameBuilder;
 import fr.ziedelth.gameframework.GameFramework;
 import fr.ziedelth.gameframework.utils.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONObject;
 
 public class JumpFight extends JavaPlugin {
+    private int taskID;
+    private JSONObject globalSettings, settings;
+    private Games game;
+    private boolean isCustom = false, isEvent = false;
+
     @Override
     public void onLoad() {
         super.onLoad();
@@ -28,7 +35,10 @@ public class JumpFight extends JavaPlugin {
 
             if ( this.globalSettings != null ) {
                 this.game = Games.getGameWithGameID(((Long) this.globalSettings.get ("name")).intValue());
-                This.isCustom = (boolean) this.global
+                this.isCustom = (boolean) this.globalSettings.get("is-custom");
+                this.isEvent = (boolean) this.globalSettings.get("is-event");
+                this.settings = (JSONObject) this.globalSettings.get("settings");
+
             }
         })
         GameBuilder gb = new GameBuilder();
@@ -37,7 +47,8 @@ public class JumpFight extends JavaPlugin {
         gb.setCanInteractWithInventory(true);
         gb.setGameSeconds(900);
         gb.addToNoDamageCause(EntityDamageEvent.DamageCause.FALL);
-        gb.setJoinMessage("§e" + ChatUtils.PLAYER_REGEX + "§7a rejoint la partie !");
+        gb.setJoinMessage("§a" + ChatUtils.PLAYER_REGEX + "§7a rejoint la partie !");
+        gb.setQuitMessage("§c" + ChatUtils.PLAYER_REGEX + "§7a quitté la partie !");
         GameFramework.setGameBuilder(gb);
     }
 }
